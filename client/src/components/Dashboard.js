fbbdbf717a8f939d9760cd8d9106357232348815
Rachel Inkala <rachelinkala@gmail.com>
2018-11-27 12:52:03 -0600

    protected route

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Redirect, Link } from 'react-router-dom';
-import { isAuthenticated } from '../fakeAuth';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 class Dashboard extends React.Component {
@@ -12,21 +11,17 @@ class Dashboard extends React.Component {
   }
 
   render() {
-    const { notes } = this.state;
-    if (isAuthenticated()) {
-      return (
-        <ul>
-          { notes.map( n =>
-              <li key={n.id}>
-                <Link to={`/notes/${n.id}`}>{n.title}</Link>
-              </li>
-            )
-          }
-        </ul>
-      )
-    } else {
-      return <Redirect to="/login" />
-    }
+    let { notes } = this.state;
+    return (
+      <ul>
+        { notes.map( n =>
+            <li key={n.id}>
+              <Link to={`/notes/${n.id}`}>{n.title}</Link>
+            </li>
+          )
+        }
+      </ul>
+    )
   }
 }
 
