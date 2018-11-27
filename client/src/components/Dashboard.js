907372a8b10e68815cb2a8d4fb03302a28d7c002
Rachel Inkala <rachelinkala@gmail.com>
2018-11-27 13:03:07 -0600

    redo dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import Form from './Form';
 
 class Dashboard extends React.Component {
-  state = { notes: [] }
+  state = { notes: [], showForm: false }
 
   componentDidMount() {
     axios.get('/api/notes')
       .then( res => this.setState({ notes: res.data }) )
   }
 
-  render() {
+  show() {
     let { notes } = this.state;
     return (
       <ul>
@@ -21,6 +22,33 @@ class Dashboard extends React.Component {
           )
         }
       </ul>
+    )
+  }
+
+  form() {
+    return <Form submit={this.submit}/>
+  }
+
+  submit = (note) => {
+    const { notes } = this.state
+    axios.post('/api/notes', { note } )
+      .then( res => this.setState({ notes: [res.data, ...notes ], showForm: false }) )
+  }
+
+  toggleForm = () => {
+    this.setState( state => {
+      return { showForm: !state.showForm }
+    })
+  }
+
+  render() {
+    const { showForm } = this.state;
+    return (
+      <div>
+        <h2>Notes</h2>
+        <button onClick={this.toggleForm}>{ showForm ? 'Hide' : 'Show' } form</button>
+        { showForm ? this.form() : this.show() }
+      </div>
     )
   }
 }
